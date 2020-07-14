6fe9a8c56f256b274d316fc2ed62c51cf284fe69
peter6q <mhce67@gmail.com>
2020-07-14 15:31:00 +0800

    <feat> Calculate the angle to wrt try_spot

--- src/locate_try_spot.py
+++ src/locate_try_spot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import math
+from math import pi
 import rospy
 import numpy as np
 import scipy.spatial as spatial
@@ -11,11 +12,15 @@ corners_pub = None
 
 
 class Line:
-    def __init__(self, x0, y0, x1, y1):
+    def __init__(self, x0, y0, x1, y1, shift_z=-pi/2):
         self.pt0 = (x0, y0)
         self.pt1 = (x1, y1)
-        self.angle_degree = abs(math.atan2(self.yDiff(), self.xDiff())*180/math.pi)
-    
+        self.shift_z = shift_z
+        self.angle_rad = math.atan2(self.yDiff(), self.xDiff()) + shift_z
+        self.angle_rad += 2*pi if self.angle_rad < -pi else 0
+        self.angle_degree_abs = abs(self.angle_rad*180/pi)
+        self.length = np.hypot(self.pt1[0]-self.pt0[0],self.pt1[1]-self.pt0[1])
+
     def xDiff(self):
         return self.pt0[0] - self.pt1[0]
 
@@ -28,6 +33,9 @@ class Line:
         if self.pt0[1] == self.pt1[1]:
             return 0.0
         return (self.pt1[1]-self.pt0[1]) / (self.pt1[0]-self.pt0[0])
+
+    def cartesian2PolarRad(self):
+        return (self.length, self.angle_rad)
 
 
 # Copied and/or modified from https://stackoverflow.com/questions/20677795/how-do-i-compute-the-intersection-point-of-two-lines
@@ -70,12 +78,20 @@ def line_segments_cb(lines_msg):
         if lines_count < 2:
             continue
         for i in range(lines_count-1):
-            if abs(extracted_line.angle_degree - lines[i].angle_degree) < 45.0:
+            if abs(extracted_line.angle_degree_abs - lines[i].angle_degree_abs) < 45.0:
                 continue
             intercept = line_intersection(extracted_line, lines[i])
             if intercept[0] is None:
                 continue
             if intercept[0]>2.0 or intercept[1]>2.0:
+                continue
+            lineA_0 = np.hypot(intercept[0]-extracted_line.pt0[0], intercept[1]-extracted_line.pt0[1])
+            lineA_1 = np.hypot(intercept[0]-extracted_line.pt1[0], intercept[1]-extracted_line.pt1[1])
+            if lineA_0 > 0.7 and lineA_1 > 0.7:
+                continue
+            lineB_0 = np.hypot(intercept[0]-lines[i].pt0[0], intercept[1]-lines[i].pt0[1])
+            lineB_1 = np.hypot(intercept[0]-lines[i].pt1[0], intercept[1]-lines[i].pt1[1])
+            if lineB_0 > 0.7 and lineB_1 > 0.7:
                 continue
             interceptions.append(intercept)
 
@@ -91,10 +107,18 @@ def line_segments_cb(lines_msg):
         result_pts.append(pts_centeroid(point_tree.data[group_idxs]) )
         clustered_pts += group_idxs
 
+    # Remove outliers
+    tmp_pts = []
+    for point in result_pts:
+        distance_to_origin = np.hypot(point[0],point[1])
+        # Only for locating try_spot
+        if distance_to_origin > 1.5:
+            continue
+        tmp_pts.append(point)
+    result_pts = tmp_pts
+
     markerArray = MarkerArray()
     for idx, point in enumerate(result_pts):
-        if np.hypot(point[0],point[1]) > 1.5: # Only for locating try_spot
-            continue
         marker = Marker()
         marker.header.frame_id = "laser"
         marker.id = idx
@@ -112,6 +136,21 @@ def line_segments_cb(lines_msg):
         marker.lifetime = rospy.rostime.Duration(0.2)
         markerArray.markers.append(marker)
     corners_pub.publish(markerArray)
+
+    if len(result_pts) < 2:
+        return
+    pts_distance = [np.hypot(point[0],point[1]) for point in result_pts]
+    pts_distance_np = np.asarray(pts_distance)
+    sorted_idx = np.argsort(pts_distance_np)
+    p0 = result_pts[sorted_idx[0]]
+    p1 = result_pts[sorted_idx[1]]
+    nearest_line = Line(p0[0], p0[1], p1[0], p1[1])
+    diff_to_try_spot_len = abs(nearest_line.length - 0.58)
+    diff_to_try_spot_wid = abs(nearest_line.length - 0.475)
+    if diff_to_try_spot_len < diff_to_try_spot_wid and diff_to_try_spot_len < 0.1:
+        print(nearest_line.cartesian2PolarRad())
+    if diff_to_try_spot_wid < diff_to_try_spot_len and diff_to_try_spot_wid < 0.1:
+        print(nearest_line.cartesian2PolarRad())
 
 
 rospy.init_node("lidar_try_spot_detector")
