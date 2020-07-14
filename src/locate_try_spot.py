772778213d65c62b4dfd22d73576e81aef88e1c9
peter6q <mhce67@gmail.com>
2020-07-14 22:37:39 +0800

    <feat> Publish try_spot location when detected

--- src/locate_try_spot.py
+++ src/locate_try_spot.py
@@ -4,20 +4,27 @@ from math import pi
 import rospy
 import numpy as np
 import scipy.spatial as spatial
+from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker, MarkerArray
+
 from laser_line_extraction.msg import LineSegment, LineSegmentList
 
 
 corners_pub = None
+base_line_pub = None
+side_line_pub = None
+try_spot_center_pub = None
+shift_z = pi/2
+tolerance = 0.05
 
 
 class Line:
-    def __init__(self, x0, y0, x1, y1, shift_z=-pi/2):
+    def __init__(self, x0, y0, x1, y1, shift_z=0):
         self.pt0 = (x0, y0)
         self.pt1 = (x1, y1)
         self.shift_z = shift_z
+        # TODO: Confine angle_rad to a particlar range
         self.angle_rad = math.atan2(self.yDiff(), self.xDiff()) + shift_z
-        self.angle_rad += 2*pi if self.angle_rad < -pi else 0
         self.angle_degree_abs = abs(self.angle_rad*180/pi)
         self.length = np.hypot(self.pt1[0]-self.pt0[0],self.pt1[1]-self.pt0[1])
 
@@ -33,6 +40,9 @@ class Line:
         if self.pt0[1] == self.pt1[1]:
             return 0.0
         return (self.pt1[1]-self.pt0[1]) / (self.pt1[0]-self.pt0[0])
+
+    def center(self):
+        return ( (self.pt0[0]+self.pt1[0])/2, (self.pt0[1]+self.pt1[1])/2 )
 
     def cartesian2PolarRad(self):
         return (self.length, self.angle_rad)
@@ -71,8 +81,8 @@ def line_segments_cb(lines_msg):
     result_pts = []
     
     for line in lines_msg.line_segments:
-        extracted_line = Line(line.start[0],line.start[1],
-            line.end[0],line.end[1])
+        extracted_line = Line(line.start[0], line.start[1],
+            line.end[0], line.end[1], shift_z)
         lines.append(extracted_line)
         lines_count += 1
         if lines_count < 2:
@@ -103,7 +113,7 @@ def line_segments_cb(lines_msg):
     for i in range( len(intercepts_np) ):
         if i in clustered_pts:
             continue
-        group_idxs = point_tree.query_ball_point(intercepts_np[i], 0.1)
+        group_idxs = point_tree.query_ball_point(intercepts_np[i], tolerance)
         result_pts.append(pts_centeroid(point_tree.data[group_idxs]) )
         clustered_pts += group_idxs
 
@@ -144,17 +154,54 @@ def line_segments_cb(lines_msg):
     sorted_idx = np.argsort(pts_distance_np)
     p0 = result_pts[sorted_idx[0]]
     p1 = result_pts[sorted_idx[1]]
-    nearest_line = Line(p0[0], p0[1], p1[0], p1[1])
+    nearest_line = Line(p0[0], p0[1], p1[0], p1[1], shift_z)
     diff_to_try_spot_len = abs(nearest_line.length - 0.58)
     diff_to_try_spot_wid = abs(nearest_line.length - 0.475)
-    if diff_to_try_spot_len < diff_to_try_spot_wid and diff_to_try_spot_len < 0.1:
-        print(nearest_line.cartesian2PolarRad())
-    if diff_to_try_spot_wid < diff_to_try_spot_len and diff_to_try_spot_wid < 0.1:
-        print(nearest_line.cartesian2PolarRad())
+    is_base_line = False
+    is_side_line = False
+    if diff_to_try_spot_len < diff_to_try_spot_wid and diff_to_try_spot_len < tolerance:
+        is_base_line = True
+        pt_polar_form = nearest_line.cartesian2PolarRad()
+        pt_to_pub = Point(pt_polar_form[0], pt_polar_form[1], 0)
+        base_line_pub.publish(pt_to_pub)
+        print(pt_polar_form)
+    if diff_to_try_spot_wid < diff_to_try_spot_len and diff_to_try_spot_wid < tolerance:
+        is_side_line = True
+        pt_polar_form = nearest_line.cartesian2PolarRad()
+        pt_to_pub = Point(pt_polar_form[0], pt_polar_form[1], 0)
+        side_line_pub.publish(pt_to_pub)
+        print(pt_polar_form)
+    if len(result_pts) < 3:
+        return
+    if is_base_line or is_side_line:
+        for idx in sorted_idx[2:]:
+            line = Line(result_pts[idx][0], result_pts[idx][1], p0[0], p0[1], shift_z)
+            diff_to_try_spot_side = abs(line.length - 0.475) if is_base_line else \
+                abs(line.length - 0.58)
+            diff_to_try_spot_hypot = abs(line.length - 0.7497) # sqrt(0.58**2 + 0.475**2)
+            try_spot_center = None
+            if diff_to_try_spot_hypot < tolerance:
+                try_spot_center = line.center()
+            if diff_to_try_spot_side < tolerance:
+                line = Line(result_pts[idx][0], result_pts[idx][1], 
+                    p1[0], p1[1], shift_z)
+                try_spot_center = line.center()
+            if try_spot_center is None:
+                continue
+            length_to_center = np.hypot(try_spot_center[0],try_spot_center[1])
+            # TODO: Confine angle_rad to a particlar range
+            angle_rad = math.atan2(-try_spot_center[1], -try_spot_center[0]) + shift_z
+            # print((length_to_center, angle_rad))
+            pt_to_pub = Point(length_to_center, angle_rad, 0)
+            try_spot_center_pub.publish(pt_to_pub)
+            break
 
 
 rospy.init_node("lidar_try_spot_detector")
-corners_pub = rospy.Publisher("/try_spot_corners", MarkerArray, queue_size=1)
+corners_pub = rospy.Publisher("/corners_marker", MarkerArray, queue_size=1)
+base_line_pub = rospy.Publisher("/try_spot_base_line", Point, queue_size=1)
+side_line_pub = rospy.Publisher("/try_spot_side_line", Point, queue_size=1)
+try_spot_center_pub = rospy.Publisher("/try_spot_center", Point, queue_size=1)
 rospy.Subscriber("/line_segments", LineSegmentList, line_segments_cb)
 
 
