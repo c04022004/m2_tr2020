ce21777a1964c69f8763ac15d8caef1f496755a2
Cyrus Chan <c04022004@gmail.com>
2020-07-21 22:38:20 +0800

    <feat> add cancel action to ps4_handler

--- src/ps4_handler.py
+++ src/ps4_handler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from geometry_msgs.msg import Twist
+from actionlib_msgs.msg import GoalID
 from m2_chassis_utils.msg import ChannelTwist
 from m2_ps4.msg import Ps4Data
 from std_msgs.msg import Bool
@@ -19,19 +20,26 @@ def ps4_cb(ps4_data): # update ps4 data
         direct.linear.x = max_linear_speed * ps4_data.hat_lx * -1
         direct.angular.z = max_rotational_speed * ps4_data.hat_rx
         vel_pub.publish(direct)
+        tr_cancel_pub.publish(GoalID())
+        sw_cancel_pub.publish(GoalID())
 
     global old_data
-    if ps4_data.triangle and not old_data.triangle: # dpad up
+    # try latch release/retract (io_7)
+    if ps4_data.triangle and not old_data.triangle:
+        io_pub.publish(1)
         io_pub.publish(1)
     if ps4_data.cross and not old_data.cross: # dpad down
+    if ps4_data.cross and not old_data.cross:
         io_pub.publish(0)
-    old_data = ps4_data
+        io_pub.publish(0)
 
 rospy.init_node('ps4_vel_controller')
 vel_pub = rospy.Publisher('/chan_cmd_vel', ChannelTwist, queue_size = 1)
+tr_cancel_pub = rospy.Publisher('/tr_server/cancel', GoalID, queue_size = 1)
+sw_cancel_pub = rospy.Publisher('/Switch/cancel', GoalID, queue_size = 1)
 ps4_sub = rospy.Subscriber('input/ps4_data', Ps4Data, ps4_cb)
 
-io_pub = rospy.Publisher('io_2/set_state', Bool, queue_size=1)
+io_pub = rospy.Publisher('io_7/set_state', Bool, queue_size=1)
 
 rospy.spin()
 
