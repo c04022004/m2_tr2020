9c158338121385818c5a68318242cf79135db887
SOFe <sofe2038@gmail.com>
2020-08-29 16:59:37 +0800

    Fixed blue led inversion

--- src/led_ctrl.py
+++ src/led_ctrl.py
@@ -3,13 +3,17 @@
 import rospy
 from std_msgs.msg import Bool
 from nav_msgs.msg import Odometry
+from configs.fieldConfig import phrase_team_from_launch, phrase_color_from_launch
+from configs.fieldConfig import ROBOT_TR1, ROBOT_TR2, MATCH_RED, MATCH_BLUE
 import time
 
 def odom_cb(odom):
+    TEAM_PUBLISHER.publish(data=True)
+    rospy.loginfo("max odom pose covariance: {}".format(max(odom.pose.covariance)))
     if max(odom.pose.covariance) >= 1000:
         # detected pose error
-        data = time.time() % 2 == 0
-        INVERT_PUBLISHER.publish(data=data)
+        sec_trunc = time.time() % 1.0
+        INVERT_PUBLISHER.publish(data=sec_trunc >= 0.5)
     else:
         INVERT_PUBLISHER.publish(data=False)
 
@@ -17,25 +21,29 @@ def team_color_topic(invert = False):
     if LAUNCH_TEAM == ROBOT_TR1:
         if (LAUNCH_COLOR == MATCH_RED) != invert: # equality xor inversion
             return "/io_board1/io_0/set_state"
-        if LAUNCH_COLOR == MATCH_BLUE: # equality xor inversion
+        if (LAUNCH_COLOR == MATCH_BLUE) != invert: # equality xor inversion
             return "/io_board1/io_1/set_state"
     if LAUNCH_TEAM == ROBOT_TR2:
         if (LAUNCH_COLOR == MATCH_RED) != invert: # equality xor inversion
             return "/io_board1/io_6/set_state"
-        if LAUNCH_COLOR == MATCH_BLUE: # equality xor inversion
+        if (LAUNCH_COLOR == MATCH_BLUE) != invert: # equality xor inversion
             return "/io_board1/io_7/set_state"
 
     raise Exception("Unknown team/color")
 
 if __name__ == "__main__":
+    rospy.init_node("led_ctrl")
+
     LAUNCH_COLOR = phrase_color_from_launch()
     LAUNCH_TEAM = phrase_team_from_launch()
     chassis_odom_topic = rospy.get_param("~chassis_odom_topic", "chassis_odom")
     rospy.Subscriber(chassis_odom_topic, Odometry, odom_cb)
 
+    rospy.loginfo("team_color_topic: {}".format(team_color_topic()))
     TEAM_PUBLISHER = rospy.Publisher(team_color_topic(), Bool, queue_size=1)
     INVERT_PUBLISHER = rospy.Publisher(team_color_topic(invert=True), Bool, queue_size=1)
 
+    time.sleep(0.5)
     TEAM_PUBLISHER.publish(data=True)
     INVERT_PUBLISHER.publish(data=False)
 
